3e1d3ee1895c9ef5a69ef1f4d172e226309a8345
Bedirhan Aydın <58737803+bedirhanayydin@users.noreply.github.com>
2023-09-08 14:35:14 +0300

    Initial Commit

---
+++ IGClone/IGCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGCloneApp.swift
+//  IGClone
+//
+//  Created by bedirhan on 8.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct IGCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
